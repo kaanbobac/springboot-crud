65aea39a48f6fe928aa42d4d78193f70bdd559d3
Kaan Bobac <bobackaan@gmail.com>
2021-05-08 17:54:06 +0300

    adjusted app properties

--- src/test/java/kaan/demo/spring/crudapp/integration/QueryAllUsersTest.java
+++ src/test/java/kaan/demo/spring/crudapp/integration/QueryAllUsersTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,7 @@ import kaan.demo.spring.crudapp.repo.UserRepository;
 @AutoConfigureMockMvc
 @Testcontainers
 @SpringBootTest
+@ActiveProfiles("test")
 public class QueryAllUsersTest {
 	@Autowired
 	private UserRepository userRepo;
