6cc764b41a4334ff8f3744098048449ee1d9d4e1
Kaan Bobac <bobackaan@gmail.com>
2021-05-09 10:33:30 +0300

    added save query apis for user

--- src/main/java/kaan/demo/spring/crudapp/model/Post.java
+++ src/main/java/kaan/demo/spring/crudapp/model/Post.java
@@ -2,11 +2,15 @@ package kaan.demo.spring.crudapp.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document("posts")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Post {
 	private int id;
